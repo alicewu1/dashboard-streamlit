016986499ea6178d6e4895f35ec4c49bdcabfc83
Ubuntu <alice@streamlit-scratch.we510rm0y0ge1mafjfkcx0w3wc.bx.internal.cloudapp.net>
2022-11-16 23:00:57 +0000

    Edited app.py

--- app.py
+++ app.py
@@ -9,15 +9,33 @@ diabetes = pd.read_csv('diabetes.csv')
 diabetes = diabetes.head(100)
 
 ## Header ##
-st.header('Diabetes Prediction Among Females')
-
+st.header('Diagnostic Measurements for Predicting Diabetes Among Females')
+st.subheader('By: Alice Wu HHA507 Data Science at Stony Brook University')
 ## Caption ##
-st.caption('This dataset is originally from the National Institute of Diabetes and Digestive and Kidney Diseases. The objective of the dataset is to diagnostically predict whether a patient has diabetes, based on certain diagnostic measurements included in the dataset. Several constraints were placed on the selection of these instances from a larger database. In particular, all patients here are females at least 21 years old of Pima Indian heritage.2')
+st.caption('This dashboard uses a dataset originally from the National Institute of Diabetes and Digestive and Kidney Diseases. The objective of the dataset is to diagnostically predict whether a patient has diabetes, based on certain diagnostic measurements included in the dataset. Constraints of the Dataset: (1)All patients here are females and at least 21 years old of Pima Indian heritage.')
 
 ## Toggle ##
 if st.checkbox('Show first 100 records of Diabetes Prediction Dataset'):
     st.dataframe(diabetes)
 
+## Code Block  ##
 
-## Charts ##
+## Dataframe and Chart 1: BMI Levels ##
+st.subheader('BMI Levels Among Patients')
 
+diabetes = pd.read_csv('diabetes.csv')
+
+BMI = diabetes['BMI'].value_counts()
+
+st.bar_chart(BMI)
+
+st.caption("This bar chart illustrates the count of unique BMI levels among female patients.")
+
+## DataFrame and Chart 2: Vitals Among Patients ##
+st.subheader('Vitals Levels Among Patients')
+
+chart_data = pd.DataFrame(np.random.randn(20, 4), columns=['Glucose','Insulin','BloodPressure', 'Pregnancies'])
+
+st.line_chart(chart_data)
+
+st.caption("This line chart illustrates to vitals levels of female patients used as diagnostic measurements to predict diabetes.")
